185ac1818d01b4e7538535d1e4abc2b9f3483d5f
ninaanatchkova <rinsingwind@gmail.com>
2018-02-02 19:38:24 +0200

    concordances

--- nltk_experiments.py
+++ nltk_experiments.py
@@ -40,11 +40,11 @@ def clean_text(path):
     text = nltk.Text(words)
     return text
 
-def get_top_words_from_csv(path):
+def get_top_words_from_csv(path, number_of_words):
     reader = csv.reader(open(path, 'r', encoding= "utf-8"))
     freq_dictionary = {}
     next(reader)
-    for row in itertools.islice(reader, 50):
+    for row in itertools.islice(reader, number_of_words):
         k, v = row
         freq_dictionary[k] = v
     word_list = []
@@ -52,10 +52,10 @@ def get_top_words_from_csv(path):
         word_list.append(key)
     return word_list
 
-def get_top_words_from_bg_freq_dict():
+def get_top_words_from_bg_freq_dict(number_of_words):
     reader = csv.reader(open('datasets/D-Fiction0001_byFreq.csv', 'r', encoding= "utf-8"))
     freq_dictionary = {}
-    for row in itertools.islice(reader, 50):
+    for row in itertools.islice(reader, number_of_words):
         row_text = row
         k_v = row_text[0].split("\t")
         k = k_v[0]
@@ -67,25 +67,57 @@ def get_top_words_from_bg_freq_dict():
     return word_list
 
 def plot_dispersions_for_text(text, path):
-    top_text_words = get_top_words_from_csv(path)
-    top_dict_words = get_top_words_from_bg_freq_dict()
+    top_text_words = get_top_words_from_csv(path, 50)
+    top_dict_words = get_top_words_from_bg_freq_dict(50)
     text.dispersion_plot(top_text_words)
     text.dispersion_plot(top_dict_words)
     
 
-def plot_frequency_distribution(text):
+def plot_frequency_distribution(text, number_of_words):
     freq_dist = FreqDist(text)
-    freq_dist.plot(50, cumulative=True)
-    print(freq_dist.most_common(50))
-    print(freq_dist["уста"])
+    freq_dist.plot(number_of_words)
+    plot_freqdist_freq(freq_dist, number_of_words)
+
+
+def plot_freqdist_freq(fd, max_num=None, cumulative=False, title='Frequency plot', linewidth=2):
+
+    tmp = fd.copy()
+    norm = fd.N()
+    for key in tmp.keys():
+        tmp[key] = float(fd[key]) / norm
+
+    if max_num:
+        tmp.plot(max_num, cumulative=cumulative,
+                 title=title, linewidth=linewidth)
+    else:
+        tmp.plot(cumulative=cumulative, 
+                 title=title, 
+                 linewidth=linewidth)
+
+    return
+
+def tokenize_bg_freq_dict(number_of_words):
+    reader = csv.reader(open('datasets/D-Fiction0001_byFreq.csv', 'r', encoding= "utf-8"))
+    freq_dictionary = {}
+    for row in itertools.islice(reader, number_of_words):
+        row_text = row
+        k_v = row_text[0].split("\t")
+        k = k_v[0]
+        v = k_v[1]
+        freq_dictionary[k] = v
+    return freq_dictionary
 
 
 
 ############################# Execute functions ############################
 
-text = tokenize_text("texts/kadiiski.txt")
-print(text)
-text2 = clean_text("texts/kadiiski.txt")
-print(text2)
+# text = tokenize_text("texts/kadiiski.txt")
+# text2 = clean_text("texts/kadiiski.txt")
 # plot_dispersions_for_text(text, "datasets/poem_set.csv")
-# plot_frequency_distribution(text)
+# plot_frequency_distribution(text2, 50)
+
+# text.concordance("свят")
+# text.concordance("слънцето")
+# text.concordance("ден")
+
+# tokenize_bg_freq_dict(50)
